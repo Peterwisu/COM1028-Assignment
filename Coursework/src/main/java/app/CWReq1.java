7b7324b27136cfa40c6734c32d16325f64b5b881
peter <peter@DESKTOP-462SJIR>
2021-04-16 23:41:44 +0100

    Add comment on CWReq1.java

--- Coursework/src/main/java/app/CWReq1.java
+++ Coursework/src/main/java/app/CWReq1.java
@@ -27,37 +27,39 @@ public class CWReq1 extends BaseQuery{
 	
 	public int getActual() throws SQLException {
 		
-
+		//Create an ArrayList of customer objects
 		ArrayList<customer> customerlist = new ArrayList<customer>();
+		//Create customer object
 		customer a;
 		
+		//Create fields of object customers
 		int customer_id;
-		
 		String first_name;
 		String last_name;
-		
 		int address_id;
 		
 		
-		
+		//Use ResultSet to retriving data from a database
 		ResultSet rs1 = this.getResultSet("Select * from customer");
+				
+		//store a data into ArrayList
 		while(rs1.next()) {
 			
+			//retrive a data from a column name
 			customer_id=rs1.getInt("customer_id");
-		
 			first_name=rs1.getString("first_name");
 			last_name=rs1.getNString("last_name");
-			
 			address_id=rs1.getInt("address_id");
 			
 			
 			
-			
+			//create a new customer object insert a data into a customer 
 			a = new  customer(customer_id,first_name,last_name,address_id);
+			//add customer into an ArrayList
 			customerlist.add(a);
 		}
 		
-		
+		//Return a size of a ArrayList using size() function which indicating a total number of customer
 		return customerlist.size();
 	}
 	
